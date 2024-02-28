510ebcecb3910f8475fb021422359a2b70d58f37
Elani Botha <lanibotha9@gmail.com>
2024-02-28 15:17:15 +0100

    Updated all folders
    setup done for models

---
+++ Data/Models/Game.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AmersfoortBoardgameEmporium.Data.Models
+{
+    public class Game
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string ShortDescription { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string ImageFile { get; set; }
+    }
+}
